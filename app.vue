e46c15fc5c56606f764a2faddda45ef83f696290
Aaron Steed <asteed7@gmail.com>
2023-12-07 12:54:52 +0000

    version 1 of the timeline

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <landing-section roles="Software Engineer 💻"></landing-section>
-    <div class="overflow-hidden min-h-screen cb-circuit-board"></div>
+    <div class="overflow-hidden min-h-screen cb-circuit-board">
+      <timeline></timeline>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import LandingSection from '~/components/sections/LandingSection.vue'
+import Timeline from '~/components/timeline.vue'
 </script>
