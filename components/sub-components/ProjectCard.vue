6abc745d8f2b336ca112b988b031ef7eef070be5
Aaron Steed <asteed7@gmail.com>
2024-12-25 02:44:03 +0000

    Projects Section

---
+++ components/sub-components/ProjectCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
+  >
+    <div
+      class="mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg"
+      :style="{ backgroundImage: `url('${image}')`, backgroundSize: 'cover' }"
+    ></div>
+    <div class="p-6 h-3/6">
+      <h5
+        class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased"
+      >
+        {{ projectName }}
+      </h5>
+      <p
+        class="block font-sans text-base font-light leading-relaxed text-inherit antialiased"
+      >
+        {{ projectDescription }}
+      </p>
+    </div>
+    <div class="p-6 pt-0">
+      <button
+        data-ripple-light="true"
+        type="button"
+        class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        @click="clicked"
+      >
+        Github ↗
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ProjectCard',
+  props: {
+    projectName: {
+      type: String,
+      required: true,
+      default: () => '',
+    },
+    projectDescription: {
+      type: String,
+      required: true,
+      default: () => '',
+    },
+    githubLink: {
+      type: String,
+      required: true,
+      default: () => '',
+    },
+    icon: {
+      type: String,
+      required: true,
+      default: () => '',
+    },
+  },
+  setup(props) {
+    function clicked(_: Event) {
+      open(props.githubLink, '_blank')
+    }
+    return { clicked, image: `/images/${props.icon}` }
+  },
+}
+</script>
---
+++ components/views/ProjectsView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="pb-20">
+    <!--    TODO maybe make this a component for all sections (a lot of it is copied in a bunch of places) -->
+    <h1
+      class="flex flex-col desktop:text-7xl tablet:text-6xl mobile:text-4xl font-bold text-center justify-center items-center text-black pt-24 pb-20"
+    >
+      Projects
+    </h1>
+    <div v-if="!isSmallScreen" class="flex justify-center space-x-40">
+      <project-card
+        github-link="https://github.com/aaronsteed/cherryblossom"
+        project-description="The site you're currently looking at. All open source and ready to be critiqued"
+        project-name="Cherry Blossom"
+        icon="cherry-blossom.jpeg"
+      >
+      </project-card>
+      <project-card
+        github-link="https://github.com/aaronsteed/technitium-dns-kube-controller"
+        project-description="Kubernetes controller to create DNS records in Technitium DNS using Custom Resource Definitions"
+        project-name="Technitium DNS Kubernetes Controller"
+        icon="kubernetes.jpg"
+      >
+      </project-card>
+    </div>
+    <div v-if="isSmallScreen" class="flex justify-center">
+      <div class="space-y-20 flex-col">
+        <project-card
+          github-link="https://github.com/aaronsteed/cherryblossom"
+          project-description="The site you're currently looking at. All open source and ready to be critiqued"
+          project-name="Cherry Blossom"
+          icon="cherry-blossom.jpeg"
+        >
+        </project-card>
+        <project-card
+          github-link="https://github.com/aaronsteed/technitium-dns-kube-controller"
+          project-description="Kubernetes controller to create DNS records in Technitium DNS using Custom Resource Definitions"
+          project-name="Technitium DNS Kubernetes Controller"
+          icon="kubernetes.jpg"
+        >
+        </project-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { useMediaQuery } from '@vueuse/core'
+import ProjectCard from '~/components/sub-components/ProjectCard.vue'
+export default {
+  name: 'ProjectsView',
+  components: { ProjectCard },
+  setup() {
+    const isSmallScreen = useMediaQuery('(max-width: 800px)')
+    return {
+      isSmallScreen,
+    }
+  },
+}
+</script>
